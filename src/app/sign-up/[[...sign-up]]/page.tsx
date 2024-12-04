4fd3ae7fef9012ae4180e832d1bbd82cfca2d8e8
suhayb <iamsuhaybahmed@gmail.com>
2024-12-04 11:24:46 +0530

    fix: sign up

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex w-full justify-center items-center">
+      <SignUp></SignUp>
+    </div>
+  );
 }
